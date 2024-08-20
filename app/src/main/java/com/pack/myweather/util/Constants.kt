954db0a2f75debccda3fbc43129ef6f1f803f31d
YoussofEshrah <youssof_eshrah@icloud.com>
2024-08-20 13:39:55 +0300

    added note for api key

--- app/src/main/java/com/pack/myweather/util/Constants.kt
+++ app/src/main/java/com/pack/myweather/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val BASE_URL = "https://api.weatherapi.com/v1/"
         const val API_KEY = "e777e8b26b804eb796e74610240708"
+    // you will need to make a new account at https://www.weatherapi.com/ and use its apikey as this key probably expired
     }
 }
